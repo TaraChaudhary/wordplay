52ae3afb87efee455ca78a118fa74d6e23eeab21
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2019-12-17 19:42:34 +0545

    updated

--- app/src/main/java/com/example/myapplication/WordActivity.java
+++ app/src/main/java/com/example/myapplication/WordActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -13,93 +16,113 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.Myadapter.Myadapter;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class WordActivity extends AppCompatActivity {
-    ListView listView;
+public class WordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String[] words = {"apple", "orange", "banana", "mango"};
+    private EditText etWord;
+    SharedPreferences sharedPreferences;
+    private RecyclerView recyclerView;
     TextView textView;
-    Button button, clr;
-    int level=0;
-    SharedPreferences sp;
-    SharedPreferences.Editor editor;
-    String un,pw;
-    String[] words={"apple","orange","grapes"};
+    private int level = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word);
+        textView =findViewById(R.id.leveltext);
+        Button btnOK = findViewById(R.id.btnOK);
+        Button btnClean = findViewById(R.id.btnClean);
+        etWord = findViewById(R.id.etWord);
+        recyclerView = findViewById(R.id.recyclerView);
 
-        sp = getApplicationContext().getSharedPreferences("mysp",MODE_PRIVATE);
-        editor = sp.edit();
+//        listWords = findViewById(R.id.listWords);
+//        showWord(level);
+        SharedPreferences savedata = getSharedPreferences("", Context.MODE_PRIVATE);
+        if (savedata.getInt("Level",0)==0) {
+            showWord(level);
+        }
+        else {
+            level=savedata.getInt("Level",0);
+            showWord(level);
+        }
 
 
-        listView = findViewById(R.id.wordlist);
-        textView = findViewById(R.id.ans);
-        button = findViewById(R.id.btn);
-        clr=findViewById(R.id.clear);
+        btnOK.setOnClickListener(this);
+        btnClean.setOnClickListener(this);
+
+//        listWords.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                etWord.append(parent.getItemAtPosition(position).toString());
+//            }
+//        });
+    }
+
+    private Character[] shuffleWords(int level){
+        char[] word = words[level].toCharArray();
 
 
+        ArrayList<Character> chars = new ArrayList<>(word.length);
+        for(char c: word){
+            chars.add(c);
+        }
 
-        ShowWord(level);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String usr_word=textView.getText().toString();
-                if (usr_word.equals(words[level])) {
-                    level++;
-                    ShowWord(level);
-                    textView.setText("");
+        Collections.shuffle(chars);
+        Character[] shuffledWord = new Character[chars.size()];
+
+        for(int i=0; i<shuffledWord.length; i++ ){
+            shuffledWord[i] = chars.get(i);
+        }
+        return shuffledWord;
+    }
+
+    private void showWord(int i){
+        Myadapter myadapter = new Myadapter(WordActivity.this, shuffleWords(i),etWord);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 3);
+        recyclerView.setAdapter(myadapter);
+        recyclerView.setLayoutManager(layoutManager);
+        textView.setText("LEVEL : " + (i+1));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnOK:
+                String usr_word = etWord.getText().toString();
+                if(level<words.length) {
+                    if (usr_word.equals(words[level])) {
+                        level++;
+                        showWord(level);
+                        sharedPreferences = getSharedPreferences("Game",MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("Level",level);
+                        editor.commit();
+                        etWord.setText("");
+                        Toast.makeText(WordActivity.this, "Next Level", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(WordActivity.this, "Wrong Word", Toast.LENGTH_SHORT).show();
+                        etWord.setText("");
+                        showWord(level);
+                    }
                 }
                 else {
-                    Toast.makeText(WordActivity.this, "Wrong", Toast.LENGTH_SHORT).show();
+                    level=0;
+                    Toast.makeText(WordActivity.this, "Game Over", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
+                break;
 
-        clr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("");
-            }
-        });
-
-    }
-    public void ShowWord(int i){
-        Character[] word=shuffleWord(words[i]);
-
-        ArrayAdapter<Character> adapter=new ArrayAdapter<Character>(this, R.layout.spinner_values,word);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String letter=parent.getItemAtPosition(position).toString();
-                textView.append(letter);
-            }
-        });
-
-    }
-
-    private Character[] shuffleWord(String word)
-    {
-        ArrayList<Character> chars=new ArrayList<>(word.length());
-        for(char c:word.toCharArray())
-        {
-            chars.add(c);
-
+            case R.id.btnClean:
+                etWord.getText().clear();
+                showWord(level);
+                break;
         }
-        Collections.shuffle(chars);
-        Character[] shuffeled=new Character[chars.size()];
-        for (int i=0;i<shuffeled.length; i++)
-        {
-            shuffeled[i]=chars.get(i);
-
-        }
-        //  String suffeledword=new String(shuffeled);
-        return shuffeled;
-
     }
 }
